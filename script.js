8a92ee568ea2f2b7a9ad71fd0e36d18b88d22d48
Benita Benny <166370745+benitabenny@users.noreply.github.com>
2025-01-03 14:49:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Alert message when the page loads
+/*alert("Welcome to My Portfolio!");
+// Change the background color on button click
+function changeBgColor() {
+document.body.style.backgroundColor = "#ffebcd";
+}
+// Add a button to the "About Me" section dynamically
+const aboutSection = document.getElementById("connect");
+const button = document.createElement("button");
+button.innerText = "Enter Name";
+button.onclick = ;
+aboutSection.appendChild(button);
+// Console log for practice
+console.log("JavaScript is connected and running!");*/
